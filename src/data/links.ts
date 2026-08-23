b1ffd43d215d6b4c67c9b388925db88de3de3c90
NayeemAROI <170694692+NayeemAROI@users.noreply.github.com>
2026-08-23 19:13:37 +0600

    fix: update Upwork profile URL

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,6 @@
 export const links = {
   github: "https://github.com/NayeemAROI",
-  upwork: "https://www.upwork.com/freelancers/~01d51a6697b0a70bb9", // User Upwork profile link
+  upwork: "https://www.upwork.com/freelancers/~019488657b8cbe5788?mp_source=share",
   linkedin: "https://www.linkedin.com/in/nayeemaroi",
   email: "nayeemur.aroi@gmail.com",
 } as const;
